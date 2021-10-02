35f4c569579342d14affd333749c384f6a6c0e2d
Carlos21122000 <craxtum1@gmail.com>
2021-10-02 12:54:55 -0600

    textura

--- globe.py
+++ globe.py
@@ -1,16 +1,39 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
 
-fig = plt.figure(figsize = (10,10))
-ax = fig.add_subplot(111, projection='3d')
+def mpl_sphere(image_file):
+    img = plt.imread(image_file)
 
-u = np.linspace(0, 2 * np.pi, 100)
-v = np.linspace(0, np.pi, 100)
+    # define a grid matching the map size, subsample along with pixels
+    theta = np.linspace(0, np.pi, img.shape[0])
+    phi = np.linspace(0, 2*np.pi, img.shape[1])
 
-x = 10 * np.outer(np.cos(u), np.sin(v))
-y = 10 * np.outer(np.sin(u), np.sin(v))
-z = 10 * np.outer(np.ones(np.size(u)), np.cos(v))
-ax.plot_surface(x, y, z, rstride=4, cstride=4, color='b')
+    count = 180 # keep 180 points along theta and phi
+    theta_inds = np.linspace(0, img.shape[0] - 1, count).round().astype(int)
+    phi_inds = np.linspace(0, img.shape[1] - 1, count).round().astype(int)
+    theta = theta[theta_inds]
+    phi = phi[phi_inds]
+    img = img[np.ix_(theta_inds, phi_inds)]
 
-plt.show()
+    theta,phi = np.meshgrid(theta, phi)
+    R = 1
+
+    # sphere
+    x = R * np.sin(theta) * np.cos(phi)
+    y = R * np.sin(theta) * np.sin(phi)
+    z = R * np.cos(theta)
+
+    # create 3d Axes
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(x.T, y.T, z.T, facecolors=img/255, cstride=1, rstride=1) # we've already pruned ourselves
+
+    # make the plot more spherical
+    ax.axis('scaled')
+
+
+if __name__ == "__main__":
+    image_file = 'bluemarble.jpg'
+    mpl_sphere(image_file)
+    plt.show()
