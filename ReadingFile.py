245c37d4cdad177e0a3aeef2d81c27774fb98f57
Mar19845 <mar19845@uvg.edu.gt>
2021-10-02 00:13:59 -0600

    add little change to reading files fucn

--- ReadingFile.py
+++ ReadingFile.py
@@ -9,14 +9,13 @@ def ReadFile(file):
             prefix, value = line.split(' ', 1)
 
             if prefix == '1':
-                tuples_list.append([value.rstrip(), ""])
+                tuples_list.append([prefix+' '+value.rstrip(), ""])
 
             elif prefix == '2':
-                tuples_list[cont][1] = value.rstrip()
+                tuples_list[cont][1] = prefix+' '+value.rstrip()
                 cont +=1
 
-    print(tuples_list)
+    #print(tuples_list)
 
     f.close()
-
-ReadFile('IRIDIUM33.txt')
+    return tuples_list
