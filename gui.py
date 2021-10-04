a527761e225364860737ee5951e52c3c896a3f4e
eduardorh1312 <53280540+eduardorh1312@users.noreply.github.com>
2021-10-03 20:34:33 -0600

    UPDATE

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from PIL import ImageTk, Image
 
 window = tk.Tk()
-window.geometry("600x600")
+window.geometry("600x400")
 window.resizable(0,0)
 window.title("Mapping Space Trash in Real Time")
 ico = tk.PhotoImage(file = 'sat.png')
@@ -22,8 +22,18 @@ e1 = tk.Entry(window).place(x=150, y=200, width=60, height=20)
 e2 = tk.Entry(window).place(x=275, y=200, width=60, height=20)
 e3 = tk.Entry(window).place(x=400, y=200, width=60, height=20)
 
+satelites = [
+    "COSMOS", 
+    "FENGYUN", 
+    "IRIDIUM33", 
+    "MICROSAT"
+    ]
 
+satelite = tk.StringVar(window)
+satelite.set(satelites[0])
+sat_menu = tk.OptionMenu(window, satelite, *satelites)
+sat_menu.place(x=255, y=250, width=100)
 
-
+tk.Button(window, text = "Map", width=10, bg='#25387d', fg='white').place(x=265,y=300)
 
 window.mainloop()
